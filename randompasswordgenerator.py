bd2705d72bae03d536604a95e61c62e8f3924edb
Guhan <113875220+Guhan300@users.noreply.github.com>
2024-07-02 10:13:21 +0530

    Random_password_generator.py

--- randompasswordgenerator.py
+++ randompasswordgenerator.py
@@ -1,10 +1,13 @@
 import random
-n=int(input('Enter the number'))
-passs=''
 
-
-a='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*-_+=<>?/'
-for i in range(n):
-    passs=passs+random.choice(a)
-    print(passs)
+user_input=int(input('Enter the number:'))
+if (user_input <8):
+    print("Minimum range must be Eight!")
+else:
+    password=""
+    keys='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*-_+=<>?/'
+    for i in range(user_input):
+        password=password+random.choice(keys)
+    print(password)
     
+
